cc34756188fe634fe8085cbf7bf7eb8d7902132f
Daniel Craig <daniel@danielcraig.me>
2014-10-03 14:23:29 +0100

    remove old migrations with colons

--- db_migrations/2014-10-01_17:31_add_bimsync_id_field_to_projects.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE projects ADD COLUMN bimsync_id VARCHAR(255)
-UPDATE projects SET bimsync_id='281d698b42174ffca1799eac19869385' WHERE name='Cambridge Science Park';
--- db_migrations/2014-10-01_17:33_fix_model_viewer_classname.sql
+++
@@ -1,2 +0,0 @@
-UPDATE apps SET appfilepath='modelviewer_app_1408716236.php' WHERE name='Model Viewer';
-UPDATE apps SET classname='modelviewer_app' WHERE name='Model Viewer';
