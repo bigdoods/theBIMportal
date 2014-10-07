c80da5cae75aed3c51092b23595d8fbc190793a3
Daniel Craig <daniel@danielcraig.me>
2014-10-07 15:15:27 +0100

    turn off browser message

--- application/views/common/incompatible-browser.php
+++ application/views/common/incompatible-browser.php
@@ -1,4 +1,4 @@
-	<?php if(!empty($_SERVER['HTTP_USER_AGENT']) && !preg_match('@(trident/|msie )[\d\.]+@i', @$_SERVER['HTTP_USER_AGENT'])){ ?>
+	<?php if(!empty($_SERVER['HTTP_USER_AGENT']) && preg_match('@(trident/|msie )[\d\.]+@i', @$_SERVER['HTTP_USER_AGENT'])){ ?>
 		<div id="incompatible-browser">
 			<p>We have detected you are using Internet Explorer. Many features on the BIMportal rely on the latest webGL which Internet Explorer doesn't currently support well. Please consider accessing the BIMportal using a different browser which supports the latest webGL such as Google Chrome or Mozilla Firefox.</p>
 		</div>
