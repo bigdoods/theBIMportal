32b9013931483eacf1a5604ec2f07ce84cbc4ba7
ashthornton <ashthornton0@gmail.com>
2014-10-13 01:15:11 +0100

    remove ID from app database entry to allow insertion

--- db_migrations/insert_qto_app.sql
+++ db_migrations/insert_qto_app.sql
@@ -1,2 +1,2 @@
-INSERT INTO `apps` (`id`, `name`, `description`, `classname`, `order`, `is_active`, `appfilepath`, `appiconfilepath`, `type`) VALUES
-(19, 'Quantity Takeoff', 'Quantity Takeoff App', 'qto_app', 1, 1, 'qto_app_1413119466.php', 'qto_app_1413119509.png', 3);
+INSERT INTO `apps` (`name`, `description`, `classname`, `order`, `is_active`, `appfilepath`, `appiconfilepath`, `type`) VALUES
+('Quantity Takeoff', 'Quantity Takeoff App', 'qto_app', 1, 1, 'qto_app_1413119466.php', 'qto_app_1413119509.png', 3);
